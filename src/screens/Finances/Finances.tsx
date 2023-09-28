830994678f4ed2afdbd20ca5cf92bb0b37e0df9b
Nusry Nizam <nusrynizam@gmail.com>
2023-09-28 18:16:18 +0530

    testing

--- src/screens/Finances/Finances.tsx
+++ src/screens/Finances/Finances.tsx
@@ -20,7 +20,7 @@ const Finances = () => {
   // }
 
   const openBottomSheet = () => {
-    navigate('add')
+    // navigate('add')
     Telegram.setItem('laptop', 300000)
     Telegram.PopupParams.title = 'Are you sure?'
     Telegram.PopupParams.description = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
